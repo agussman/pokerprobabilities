0cf3c92c4953b395843a04ea40d3981ab7030887
Aaron Gussman <agussman@thehumangeo.com>
2017-06-27 22:15:19 -0400

    This works

--- poker_prob.py
+++ poker_prob.py
@@ -1,10 +1,15 @@
+
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, world!", 200
+    return "Hello, world 4!", 200
 
-# We only need this for local development.
 if __name__ == '__main__':
-    app.run()
+    if 'ASK_VERIFY_REQUESTS' in os.environ:
+        verify = str(os.environ.get('ASK_VERIFY_REQUESTS', '')).lower()
+        if verify == 'false':
+            app.config['ASK_VERIFY_REQUESTS'] = False
+    app.run(debug=True)
