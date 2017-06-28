247fb0bc784c914e4c38ad5bb3f7530ad622c356
Aaron Gussman <agussman@thehumangeo.com>
2017-06-27 21:36:41 -0400

    This basic script deploys via zappa

--- poker_prob.py
+++ poker_prob.py
@@ -1,41 +1,10 @@
-import logging
-import os
-
 from flask import Flask
-from flask_ask import Ask, request, session, question, statement
-
-
 app = Flask(__name__)
-ask = Ask(app, "/")
-logging.getLogger('flask_ask').setLevel(logging.DEBUG)
 
+@app.route('/')
+def index():
+    return "Hello, world!", 200
 
-@ask.launch
-def launch():
-    speech_text = 'Welcome to the Alexa Skills Kit, you can say hello'
-    return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
-
-
-@ask.intent('PokerIntent')
-def poker_prob():
-    speech_text = 'Hello world'
-    return statement(speech_text).simple_card('HelloWorld', speech_text)
-
-
-@ask.intent('AMAZON.HelpIntent')
-def help():
-    speech_text = 'You can say hello to me!'
-    return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
-
-
-@ask.session_ended
-def session_ended():
-    return "{}", 200
-
-
+# We only need this for local development.
 if __name__ == '__main__':
-    if 'ASK_VERIFY_REQUESTS' in os.environ:
-        verify = str(os.environ.get('ASK_VERIFY_REQUESTS', '')).lower()
-        if verify == 'false':
-            app.config['ASK_VERIFY_REQUESTS'] = False
-    app.run(debug=True)
+    app.run()
